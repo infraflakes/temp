5c76ab8c1c732ec107da58053f39b08230bed352
infraflakes <infraflakes@proton.me>
2026-03-19 20:42:48 +0700

    Bump version

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        version = "0.1.9";
+        version = "0.1.10";
         pkgs = import nixpkgs {inherit system;};
         # Use pkgsStatic to get X11 libs compiled against musl
         staticPkgs = pkgs.pkgsStatic;
