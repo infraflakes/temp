85a3cd9707e6b2dfcfdaade1c3314c80a0e42b3b
infraflakes <infraflakes@proton.me>
2026-04-01 18:12:05 +0700

    Fix drag window with edge panning when not zoomed out require manual mouse movement

--- c-src/input/mouse.c
+++ c-src/input/mouse.c
@@ -21,7 +21,8 @@ void movemouse(const Arg* arg) {
   int x11_fd = ConnectionNumber(dpy);
   int got_release = 0;
   while (!got_release) {
-    int need_autopan = selmon->canvas_zoom < 1.0f;
+    extern int edge_autopan_enabled;
+    int need_autopan = edge_autopan_enabled;
 
     if (need_autopan) {
         // Non-blocking check for matching events
